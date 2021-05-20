7e683be3bb21483d333c3ded903ba3698dca0b58
Round Robin <kac4484@gmail.com>
2021-05-20 22:33:07 +0900

    Add files via upload

---
+++ CH_Server/CH_Server/CH_Server.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CH_Server
+{
+    partial class CH_Server
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CH_Server));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(314, 141);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.종료ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 26);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(252, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "종료";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(11, 208);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(312, 142);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(115, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "접속자 수 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(208, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // CH_Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(334, 365);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "CH_Server";
+            this.Opacity = 0.95D;
+            this.Text = "CH_Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CH_Server_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CH_Server/CH_Server/CH_Server.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CH_Server
+{
+    public partial class CH_Server : Form
+    {
+        TcpListener server = null;
+        TcpClient clientSocket = null;
+        static int counter = 0;
+        public int people = 0;
+        public Dictionary<TcpClient, string> clientList = new Dictionary<TcpClient, string>();
+        public int close = 0;
+        public static List<string> people_name = new List<string>();
+        public static List<string> PTEMP_name = new List<string>();
+        public static int True_False = 0;
+        public static string people_dis = "";
+
+        public CH_Server()
+        {
+            InitializeComponent();
+
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+            this.notifyIcon1.Visible = true;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+
+            // socket start
+            Thread t = new Thread(InitSocket);
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitSocket()
+        {
+            server = new TcpListener(IPAddress.Any, 9999);
+            clientSocket = default(TcpClient);
+            server.Start();
+            DisplayText(">> Server Started");
+
+            while (true)
+            {
+                try
+                {
+                    counter++;
+                    clientSocket = server.AcceptTcpClient();
+                    DisplayText(">> Accept connection from client");
+
+                    NetworkStream stream = clientSocket.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string user_name = Encoding.Unicode.GetString(buffer, 0, bytes);
+                    user_name = user_name.Substring(0, user_name.IndexOf("$"));
+
+                    clientList.Add(clientSocket, user_name);
+
+                    // send message all user
+                    SendMessageAll(user_name + " 사용자 접속 ", "", true);
+
+                    richTextBox2.AppendText(user_name+Environment.NewLine);
+                    this.richTextBox2.ScrollToCaret();
+                    people++;
+                    people_name.Add(user_name);
+                    label2.Text = Convert.ToString(people);
+
+                    handleClient h_client = new handleClient();
+                    h_client.OnReceived += new handleClient.MessageDisplayHandler(OnReceived);
+                    h_client.OnDisconnected += new handleClient.DisconnectedHandler(h_client_OnDisconnected);
+                    h_client.startClient(clientSocket, clientList);
+                }
+                catch (SocketException se)
+                {
+                    Trace.WriteLine(string.Format("InitSocket - SocketException : {0}", se.Message));
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(string.Format("InitSocket - Exception : {0}", ex.Message));
+                    break;
+                }
+            }
+
+            clientSocket.Close();
+            server.Stop();
+        }
+
+        void h_client_OnDisconnected(TcpClient clientSocket)
+        {
+            if (clientList.ContainsKey(clientSocket))
+                clientList.Remove(clientSocket);
+
+            foreach(string peo_temp in people_name)
+            {
+                if (peo_temp == people_dis)
+                {
+                    richTextBox2.AppendText(peo_temp + "님이 접속을 해제 하였습니다." + Environment.NewLine);
+
+                    people_name.Remove(peo_temp);
+                    foreach (string a in people_name)
+                    {
+                        PTEMP_name.Add(a);
+                    }
+                    people_name = new List<string>();
+
+                    foreach (string b in PTEMP_name)
+                    {
+                        people_name.Add(b);
+                    }
+
+                    people--;
+                    label2.Text = Convert.ToString(people);
+                    return;
+                }
+            }
+            //richTextBox2.AppendText("사용자 연결해제" + Environment.NewLine);
+
+        }
+
+        private void OnReceived(string message, string user_name)
+        {
+            string displayMessage = "From client : " + user_name + " : " + message;
+            DisplayText(displayMessage);
+            SendMessageAll(message, user_name, true);
+        }
+
+        public void SendMessageAll(string message, string user_name, bool flag)
+        {
+            foreach (var pair in clientList)
+            {
+                Trace.WriteLine(string.Format("tcpclient : {0} user_name : {1}", pair.Key, pair.Value));
+
+                TcpClient client = pair.Key as TcpClient;
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = null;
+
+                if (flag)
+                {
+                    buffer = Encoding.Unicode.GetBytes(user_name + System.DateTime.Now.ToString("(" + "HH:mm:ss" + ")")+ " : " + message );
+                }
+                else
+                {
+                    buffer = Encoding.Unicode.GetBytes(message);
+                }
+
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Flush();
+            }
+        }
+
+        private void DisplayText(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.BeginInvoke(new MethodInvoker(delegate
+                {
+                    richTextBox1.AppendText(text + Environment.NewLine);
+                    this.richTextBox1.ScrollToCaret();
+                }));
+            }
+            else
+                richTextBox1.AppendText(text + Environment.NewLine);
+            this.richTextBox1.ScrollToCaret();
+
+            try
+            {
+                //MessageBox.Show(text)
+                string[] temp = text.Split(':');
+
+                if (temp[3] == "/DIS")
+                {
+                    people_dis = temp[4];
+                    SendMessageAll(temp[2], "", false);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = true; // 폼의 표시
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal; // 최소화를 멈춘다 
+            this.Activate(); // 폼을 활성화 시킨다
+        }
+
+        private void CH_Server_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (close == 0)
+            {
+                e.Cancel = true; // 종료 이벤트를 취소 시킨다
+                this.Visible = false; // 폼을 표시하지 않는다;
+            }
+            else if (close == 1)
+            {
+                e.Cancel = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            close = 1;
+            Application.Exit();
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            close = 1;
+            Application.Exit();
+        }
+    }
+}
