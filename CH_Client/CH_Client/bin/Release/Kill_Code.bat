0641cc13df9ad5b55e302b0184cca63055018f7f
Round Robin <kac4484@gmail.com>
2021-05-20 22:27:02 +0900

    Delete CH_Client/CH_Client directory

--- CH_Client/CH_Client/bin/Release/Kill_Code.bat
+++
@@ -1,8 +0,0 @@
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-@echo off
-
-timeout /t 1
-echo.
-echo Kill Code Start
-
-del %~dp0CH_Client.exe
