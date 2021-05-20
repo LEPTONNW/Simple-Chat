b08fccf9314d1f55ef52da0852bd51837fa1cbb9
Round Robin <kac4484@gmail.com>
2021-05-20 22:32:37 +0900

    Add files via upload

---
+++ CH_Client/CH_Client/KILL.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CH_Client
+{
+    partial class KILL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(31, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 22);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "확인";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(160, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 22);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "취소";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
+            // 
+            // KILL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(296, 81);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "KILL";
+            this.Text = "KILL";
+            this.Load += new System.EventHandler(this.KILL_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.KILL_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
